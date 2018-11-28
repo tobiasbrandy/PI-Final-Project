2f37c4be0eba54e54c517c2b710037c4912d852e
eugepineiro <45410037+eugepineiro@users.noreply.github.com>
2018-11-28 02:58:02 -0300

    Update airportADT.h

--- Proyecto Final/TAD/airportADT.h
+++ Proyecto Final/TAD/airportADT.h
@@ -5,30 +5,30 @@ typedef struct airportCDT * airportADT;
 airportADT createAirport();
 
 /*
-* Inserta un nuevo aeropuerto a la lista. Los ordena alfabéticamente segun el OACI.
+* Inserta un nuevo aeropuerto a la lista. Los ordena alfabéticamente según el código OACI.
 */
 airportADT insertAirport(char * oaci, char * denom, char * province);
 
 void freeAirport(airportADT airport);
 
 /*
-*Crea un array de una estructura de aeropuertos reducidos, que solo tienen el OACI y la cantidad de movimientos.
+* Crea un array de una estructura de aeropuertos reducidos, que solo tienen el OACI y la cantidad de movimientos.
 */
 airportMini * airportToArray(airportADT airport, int * dim);
 
 /*
-*Recibe un array de estructuras de aeropuertos reducidos cuyo orden coincide con la lista y reemplaza los valores
-*de movimientos de la lista por los del array.
+* Recibe un array de estructuras de aeropuertos reducidos cuyo orden coincide con la lista y reemplaza los valores
+* de movimientos de la lista por los del array.
 */
 void updateMovsWithArray(airportMini * airportMini);
 
 /*
-*Crea una copia de la lista que esta ordenada segun la cantidad de movimientos de manera descendiente y secundariamente
-*por el orden del OACI.
+* Crea una copia de la lista que está ordenada según la cantidad de movimientos de manera descendente y secundariamente
+* alfabéticamente por código OACI
 */
 //static airportADT sortByMovs(airportADT airport); NO TIENE QUE IR EN EL .H PORQUE ES STATIC.
 
 /*
-*Almacena en un archivo de texto los aeropuertos juntos con la cantidad de movimientos de manera descendiente.
+* Almacena en un archivo de texto los aeropuertos junto con la cantidad de movimientos de manera descendente.
 */
 int storageByMovs(airportADT airport);

67ae497f15ac3492d773e4903764d2395dcc14ac
tobiasbrandy <tobiasbrandy@hotmail.com>
2018-11-30 13:12:07 -0300

    Implementado almacenamiento

--- ProyectoFinal/main.c
+++ ProyectoFinal/main.c
@@ -24,13 +24,21 @@ int main(/* int numOfFiles, dataType data[] */) // data[0] almacena el nombre, d
 		freeMovements(mv);
 	}
 
-	storeAirportsByMovs(ap); //Almacena en el archivo correspondiente la Query 1.
-	storeMovsByWeekdayAndClasif(week); //Almacena en el archivo correspondiente la Query 2.
-	storeMovsByClasifAndClass(moveComp); //Almacena en el archivo correspondiente la Query 3.
+
+	//Almacena en el archivo correspondiente la Query 1.
+	storeAirportsByMovs(ap);
+
+	//Almacena en el archivo correspondiente la Query 2.
+	storeMovsByWeekdayAndClasif(week);
+
+	//Almacena en el archivo correspondiente la Query 3.
+	storeMovsByClasifAndClass(moveComp);
 
 	freeAirport(ap);
 	freeMatrix((void**)week, 7);
 	freeMatrix((void**)moveComp, NOCLASS);
+
+
 
 	//FINAL MAIN. YA ESTA!!!!!!
 	*/
@@ -39,8 +47,85 @@ int main(/* int numOfFiles, dataType data[] */) // data[0] almacena el nombre, d
 	stringvToFile (vectorStrings, movimientos_aeropuestos, .csv) // cada query la va a llamar una vez, permite elegir la extensión del archivo
 */
 
+	//PARA TESTEAR!!
+	/*airportADT ap = createAirport();
 
+	insertAirport(ap, "SAME", "ezeiza", "lanus");
+	insertAirport(ap, "AMBE", "ezeiza", "lanus");
+	insertAirport(ap, "FOEP", "ezeiza", "lanus");
+	insertAirport(ap, "FISH", "ezeiza", "lanus");
+	insertAirport(ap, "PFHT", "ezeiza", "lanus");
+	insertAirport(ap, "MVGS", "ezeiza", "lanus");
+	insertAirport(ap, "PAID", "ezeiza", "lanus");
+	insertAirport(ap, "MFHT", "ezeiza", "lanus");
+	insertAirport(ap, "APWE", "ezeiza", "lanus");
+	insertAirport(ap, "MNDG", "ezeiza", "lanus");
+	insertAirport(ap, "PEID", "ezeiza", "lanus");
+	insertAirport(ap, "QPWO", "ezeiza", "lanus");
+	insertAirport(ap, "FHDS", "ezeiza", "lanus");
+	insertAirport(ap, "FHGE", "ezeiza", "lanus");
+	insertAirport(ap, "FBYT", "ezeiza", "lanus");
+	insertAirport(ap, "DNYR", "ezeiza", "lanus");
+
+	startFasterAirportSearch(ap);
+
+	movementsADT mv = createMovements();
+
+	tDate date0 = {10, 6, 2000};
+	tDate date1 = {11, 6, 2000};
+	tDate date2 = {12, 6, 2000};
+	tDate date3 = {13, 6, 2000};
+	tDate date4 = {14, 6, 2000};
+	tDate date5 = {15, 6, 2000};
+	tDate date6 = {16, 6, 2000};
+
+	insertMovements(mv, date0, REGULAR, CABOTAGE, TAKEOFF, "SAME", "MFHT", "Aerolineas");
+	insertMovements(mv, date6, NOREGULAR, INTERNATIONAL, LANDING, "SAME", "QPWO", "Aerolineas");
+	insertMovements(mv, date5, REGULAR, CABOTAGE, TAKEOFF, "APWE", "DNYR", "Aerolineas");
+	insertMovements(mv, date4, PRIVATE, NA, LANDING, "FOEP", "AMBE", "Aerolineas");
+	insertMovements(mv, date3, PRIVATE, INTERNATIONAL, TAKEOFF, "QPWO", "DNYR", "Aerolineas");
+	insertMovements(mv, date2, NOREGULAR, CABOTAGE, TAKEOFF, "SAME", "AMBE", "Aerolineas");
+	insertMovements(mv, date1, REGULAR, INTERNATIONAL, TAKEOFF, "APWE", "QPWO", "Aerolineas");
+	insertMovements(mv, date0, REGULAR, CABOTAGE, LANDING, "SAME", "AMBE", "Aerolineas");
+	insertMovements(mv, date5, PRIVATE, CABOTAGE, LANDING, "APWE", "AMBE", "Aerolineas");
+	insertMovements(mv, date6, REGULAR, CABOTAGE, LANDING, "SAME", "AMBE", "Aerolineas");
+	insertMovements(mv, date6, NOREGULAR, INTERNATIONAL, TAKEOFF, "QPWO", "MFHT", "Aerolineas");
+	insertMovements(mv, date3, PRIVATE, CABOTAGE, LANDING, "SAME", "AMBE", "Aerolineas");
+	insertMovements(mv, date2, NOREGULAR, INTERNATIONAL, TAKEOFF, "MFHT", "APWE", "Aerolineas");
+	insertMovements(mv, date2, NOREGULAR, INTERNATIONAL, LANDING, "APWE", "FOEP", "Aerolineas");
+	insertMovements(mv, date0, REGULAR, INTERNATIONAL, TAKEOFF, "SAME", "AMBE", "Aerolineas");
+	insertMovements(mv, date1, REGULAR, CABOTAGE, LANDING, "APWE", "AMBE", "Aerolineas");
+	insertMovements(mv, date1, NOREGULAR, INTERNATIONAL, TAKEOFF, "DNYR", "QPWO", "Aerolineas");
+	insertMovements(mv, date4, PRIVATE, INTERNATIONAL, TAKEOFF, "MFHT", "DNYR", "Aerolineas");
+	insertMovements(mv, date4, NOREGULAR, CABOTAGE, LANDING, "MFHT", "AMBE", "Aerolineas");
+	insertMovements(mv, date0, REGULAR, INTERNATIONAL, TAKEOFF, "QPWO", "AMBE", "Aerolineas");
+
+	int ** week = (int**)createMatrix(7, NOCLASIF, sizeof(int));
+	int ** moveComp = (int**)createMatrix(NOCLASS, NOCLASIF, sizeof(int));
+
+	matrixAddition((void**)week, (void**)getMovsByWeekDay(mv), 7, NOCLASIF, sizeof(int), (void (*)(void*, void*))addInts);
+	matrixAddition((void**)moveComp, (void**)getMovsComposition(mv), NOCLASS, NOCLASIF, sizeof(int), (void (*)(void*, void*))addInts);
+
+	addBlockMovementsToAirport(mv, ap);
+
+	freeMovements(mv);
+
+	printf("Query 1\n");
+	storeAirportsByMovs(ap);
+
+	printf("Query 2\n");
+	storeMovsByWeekdayAndClasif(week);
+
+	printf("Query 3\n");
+	storeMovsByClasifAndClass(moveComp);
+
+
+	freeAirport(ap);
+	freeMatrix((void**)week, 7);
+	freeMatrix((void**)moveComp, NOCLASS);
+*/
 }
+
 
 /*void stringvToFile (char ** stringv, char * fileName, char * extent) {
 	char * validExtent, aux, validExtent;
